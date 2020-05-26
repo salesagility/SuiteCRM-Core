6d976caac779caf72471fc1503845f7c659f6085
Clemente Raposo <clemente.raposo@salesagility.com>
2020-05-26 16:47:20 +0100

    Fix module navigation jasmine test

--- core/app/src/services/navigation/module-navigation/module-navigation.service.spec.mock.ts
+++ core/app/src/services/navigation/module-navigation/module-navigation.service.spec.mock.ts
@@ -1,10 +1,8 @@
 import {ModuleNavigation} from '@services/navigation/module-navigation/module-navigation.service';
-import {NavigationExtras, Router, UrlTree} from '@angular/router';
+import {NavigationExtras, Router} from '@angular/router';
 
 const mockRouter = {
     // eslint-disable-next-line @typescript-eslint/no-unused-vars
-    navigateByUrl: (url: string | UrlTree, extras?: NavigationExtras) => {
-        return null;
-    }
+    navigate: (commands: any[], extras?: NavigationExtras) => null
 } as Router;
 export const mockModuleNavigation = new ModuleNavigation(mockRouter);
