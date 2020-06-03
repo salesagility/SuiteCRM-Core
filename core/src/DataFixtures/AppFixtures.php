b9a5095b783935884c2ae04a7722be3f827feb0a
Dillon-Brown <dillon.brown@salesagility.com>
2020-06-03 05:57:27 +0100

    Update AppFixtures to use auto-wiring
    - Fixed the Doctrine Entity Manager being passed as an argument.
    
    Signed-off-by: Dillon-Brown <dillon.brown@salesagility.com>

--- core/src/DataFixtures/AppFixtures.php
+++ core/src/DataFixtures/AppFixtures.php
@@ -3,11 +3,14 @@
 namespace App\DataFixtures;
 
 use Doctrine\Bundle\FixturesBundle\Fixture;
-use Doctrine\Common\Persistence\ObjectManager;
+use Doctrine\Persistence\ObjectManager;
 
 class AppFixtures extends Fixture
 {
-    public function load(ObjectManager $manager)
+    /**
+     * @param ObjectManager $manager
+     */
+    public function load(ObjectManager $manager): void
     {
         $manager->flush();
     }
