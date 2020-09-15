2f04c03c13ebe34430ba3a747025e39e53de58bd
codeph0y <code.ph0y@googlemail.com>
2020-09-15 09:17:14 +0100

    Add select which subpanels to view banner

--- core/app/src/components/subpanel/subpanel.component.ts
+++ core/app/src/components/subpanel/subpanel.component.ts
@@ -1,8 +1,39 @@
-import {Component} from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {LanguageStore, LanguageStrings} from '@store/language/language.store';
+import {combineLatest, Observable} from 'rxjs';
+import {map} from 'rxjs/operators';
 
 @Component({
     selector: 'scrm-subpanel',
     templateUrl: 'subpanel.component.html',
 })
-export class SubpanelComponent {
+export class SubpanelComponent implements OnInit {
+    isCollapsed: boolean = false;
+    toggleIcon: string = 'arrow_down_filled';
+    
+    languages$: Observable<LanguageStrings> = this.languageStore.vm$;
+    records$:  Observable<LanguageStrings> = this.languageStore.vm$;
+
+    vm$ = combineLatest([
+        this.languages$,
+        
+    ]).pipe(
+        map(([languages]) => (
+            {
+                appStrings: languages.appStrings || {},
+                appListStrings: languages.appListStrings || {}
+            })
+        )
+    );
+
+    constructor(protected languageStore: LanguageStore) {
+    }
+
+    ngOnInit(): void {
+    }
+
+    toggleSubPanels(): void {
+        this.isCollapsed = !this.isCollapsed;
+        this.toggleIcon = (this.isCollapsed) ? 'arrow_up_filled' : 'arrow_down_filled';
+    }
 }
--- core/app/src/components/subpanel/subpanel.module.ts
+++ core/app/src/components/subpanel/subpanel.module.ts
@@ -3,14 +3,20 @@ import {CommonModule} from '@angular/common';
 import {AppManagerModule} from '../../app-manager/app-manager.module';
 import {SubpanelComponent} from './subpanel.component';
 import {PanelModule} from '@components/panel/panel.module';
+import {ImageModule} from '@components/image/image.module';
+import {RouterModule} from '@angular/router';
+import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
 
 @NgModule({
     declarations: [SubpanelComponent],
     exports: [SubpanelComponent],
     imports: [
         CommonModule,
+        NgbModule,
+        ImageModule,
         AppManagerModule.forChild(SubpanelComponent),
         PanelModule,
+        RouterModule
     ]
 })
 export class SubpanelModule {
