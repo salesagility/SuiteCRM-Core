b4639304bb7ecb94b89b4900535e7b17a339c208
Clemente Raposo <clemente.raposo@salesagility.com>
2020-09-28 12:30:46 +0100

    Send html entities decoded data
    - Remove forced innerHtml injection and sanitize

--- core/app/fields/varchar/templates/detail/varchar.module.ts
+++ core/app/fields/varchar/templates/detail/varchar.module.ts
@@ -3,7 +3,6 @@ import {CommonModule} from '@angular/common';
 
 import {AppManagerModule} from '@base/app-manager/app-manager.module';
 import {VarcharDetailFieldComponent} from './varchar.component';
-import {HtmlSanitizeModule} from '@base/pipes/html-sanitize/html-sanitize.module';
 
 @NgModule({
     declarations: [VarcharDetailFieldComponent],
@@ -11,7 +10,6 @@ import {HtmlSanitizeModule} from '@base/pipes/html-sanitize/html-sanitize.module
     imports: [
         CommonModule,
         AppManagerModule.forChild(VarcharDetailFieldComponent),
-        HtmlSanitizeModule
     ]
 })
 export class VarcharDetailFieldModule {
