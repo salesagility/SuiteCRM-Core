24432c342c54bd4f8bc40d8d0ddf081f0adeaf61
Clemente Raposo <clemente.raposo@salesagility.com>
2020-10-15 00:45:44 +0100

    Record and list view styling adjustments
    - Add standard fontsizes and heights for panel headers
    -- user panel in widget component
    - Adjust subpanel square buttons to match mocks
    - Adjust inline edit button layout to always be on the right of the field
    - Add word wrapping to record-content fields to support long names/text

--- core/app/src/components/widget/widget.module.ts
+++ core/app/src/components/widget/widget.module.ts
@@ -2,13 +2,15 @@ import {NgModule} from '@angular/core';
 import {CommonModule} from '@angular/common';
 import {WidgetComponent} from './widget.component';
 import {ImageModule} from '@components/image/image.module';
+import {PanelModule} from '@components/panel/panel.module';
 
 @NgModule({
     declarations: [WidgetComponent],
     exports: [WidgetComponent],
     imports: [
         CommonModule,
-        ImageModule
+        ImageModule,
+        PanelModule
     ]
 })
 export class WidgetModule {
