8235e46b2d9e9670f7eb661371baa9fbbdb72fa7
Dillon-Brown <dillon.brown@salesagility.com>
2020-10-23 11:16:33 +0100

    Update Composer
    Signed-off-by: Dillon-Brown <dillon.brown@salesagility.com>

--- config/bundles.php
+++ config/bundles.php
@@ -12,5 +12,5 @@ return [
     Symfony\Bundle\MakerBundle\MakerBundle::class => ['dev' => true],
     Nelmio\CorsBundle\NelmioCorsBundle::class => ['all' => true],
     Doctrine\Bundle\MigrationsBundle\DoctrineMigrationsBundle::class => ['all' => true],
-    Symfony\Bundle\WebProfilerBundle\WebProfilerBundle::class => ['dev' => true, 'test' => true]
+    Symfony\Bundle\WebProfilerBundle\WebProfilerBundle::class => ['dev' => true, 'test' => true],
 ];
