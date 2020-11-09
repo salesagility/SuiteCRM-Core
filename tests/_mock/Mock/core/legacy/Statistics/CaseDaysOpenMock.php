aeff40fec1b99ecb39c44e17249bca91715a31a3
j.anderson <jack.anderson@salesagility.com>
2020-11-09 14:26:56 +0000

    Add tests for Case Days Open

---
+++ tests/_mock/Mock/core/legacy/Statistics/CaseDaysOpenMock.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace Mock\Core\Legacy\Statistics;
+
+use App\Legacy\Statistics\CaseDaysOpen;
+use Doctrine\ORM\EntityManagerInterface;
+use aCase;
+use Mock\Helpers\Core\Legacy\Data\DBQueryResultsMocking;
+use SugarBean;
+
+class CaseDaysOpenMock extends CaseDaysOpen
+{
+    use DBQueryResultsMocking;
+
+    /**
+     * @var aCase
+     */
+    public $case;
+
+    /**
+     * @param aCase $case
+     */
+    public function setCase(aCase $case): void
+    {
+        $this->case = $case;
+    }
+
+    /**
+     * @param $id
+     * @return aCase
+     */
+    protected function getCase(string $id): aCase
+    {
+        return $this->case;
+    }
+
+    protected function startLegacyApp(): void
+    {
+    }
+
+    /**
+     * @inheritDoc
+     */
+    protected function runAuditInfoQuery(
+        EntityManagerInterface $em,
+        SugarBean $bean,
+        string $field,
+        array $procedureParams,
+        $innerQuery
+    ): ?array {
+        return $this->getAllMockQueryResults();
+    }
+}
