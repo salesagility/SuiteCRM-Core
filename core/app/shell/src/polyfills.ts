93b53c57132d8bb4070d67a0c528b2c390973d63
Clemente Raposo <clemente.raposo@salesagility.com>
2021-06-25 12:17:45 +0100

    Angular 12 upgrade

--- core/app/shell/src/polyfills.ts
+++ core/app/shell/src/polyfills.ts
@@ -28,6 +28,10 @@
  * Load `$localize` onto the global scope - used if i18n tags appear in Angular templates.
  */
 import '@angular/localize/init';
+/***************************************************************************************************
+ * Zone JS is required by default for Angular itself.
+ */
+import 'zone.js'; // Included with Angular CLI.
 // tslint:disable
 /**
  * This file includes polyfills needed by Angular and is loaded before the app.
@@ -79,11 +83,6 @@ import '@angular/localize/init';
  * Needed for: All but Chrome, Firefox and Opera. http://caniuse.com/#feat=web-animation
  **/
 // import 'web-animations-js';  // Run `npm install --save web-animations-js`.
-
-/***************************************************************************************************
- * Zone JS is required by default for Angular itself.
- */
-import 'zone.js/dist/zone'; // Included with Angular CLI.
 
 /***************************************************************************************************
  * APPLICATION IMPORTS
