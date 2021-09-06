4075f0a13b1a3233a7e6b43c706ce6a0971e4111
Clemente Raposo <clemente.raposo@salesagility.com>
2021-09-06 11:58:56 +0100

    Declare needed external libs as shared modules
    - This is required for extensions to have access to these libs

--- core/app/shell/webpack.config.js
+++ core/app/shell/webpack.config.js
@@ -1,8 +1,8 @@
-const ModuleFederationPlugin = require("webpack/lib/container/ModuleFederationPlugin");
+const ModuleFederationPlugin = require('webpack/lib/container/ModuleFederationPlugin');
 
 module.exports = {
   output: {
-    uniqueName: "shell",
+    uniqueName: 'shell',
   },
   optimization: {
     runtimeChunk: false
@@ -13,33 +13,107 @@ module.exports = {
       remotes: {},
 
       shared: {
-        "@angular/core": {
+        '@angular/core': {
           singleton: true,
           requiredVersion: '^12.0.0'
         },
-        "@angular/common": {
+        '@angular/common': {
           singleton: true,
           requiredVersion: '^12.0.0'
         },
-        "@angular/router": {
+        '@angular/common/http': {
           singleton: true,
           requiredVersion: '^12.0.0'
+        },
+        '@angular/router': {
+          singleton: true,
+          requiredVersion: '^12.0.0'
+        },
+        '@angular/animations': {
+          singleton: true,
+          requiredVersion: '^12.0.0'
+        },
+        '@angular/cdk': {
+          singleton: true,
+          requiredVersion: '^11.2.13'
+        },
+        '@angular/cdk/table': {
+          singleton: true,
+          requiredVersion: '^11.2.13'
+        },
+        '@angular/cdk/observers': {
+          singleton: true,
+          requiredVersion: '^11.2.13'
+        },
+        '@angular/forms': {
+          singleton: true,
+          requiredVersion: '^12.0.0'
+        },
+        '@apollo/client': {
+          singleton: true,
+          requiredVersion: '^3.3.7'
+        },
+        '@apollo/link-error': {
+          singleton: true,
+          requiredVersion: '^2.0.0-beta.3'
+        },
+        'angular-svg-icon': {
+          singleton: true,
+          requiredVersion: '^12.0.0'
+        },
+        'apollo-angular': {
+          singleton: true,
+          requiredVersion: '^2.2.0'
+        },
+        graphql: {
+          singleton: true,
+          requiredVersion: '^14.7.0'
+        },
+        'graphql-tag': {
+          singleton: true,
+          requiredVersion: '^2.11.0'
+        },
+        'lodash-es': {
+          singleton: true,
+          requiredVersion: '^4.17.20'
+        },
+        luxon: {
+          singleton: true,
+          requiredVersion: '^1.25.0'
+        },
+        'ng-animate': {
+          singleton: true,
+          requiredVersion: '^1.0.0'
+        },
+        'ngx-chips': {
+          singleton: true,
+          requiredVersion: '^2.2.2'
         },
 
-        "@swimlane/ngx-charts": {
+        '@swimlane/ngx-charts': {
           singleton: true,
           requiredVersion: '^17.0.0'
         },
 
-        "common": {
+        '@ng-bootstrap/ng-bootstrap': {
           singleton: true,
-          import: "dist/common",
+          requiredVersion: '^9.0.2'
+        },
+
+        'bn-ng-idle': {
+          singleton: true,
+          requiredVersion: '^1.0.1'
+        },
+
+        common: {
+          singleton: true,
+          import: 'dist/common',
           requiredVersion: false
         },
 
-        "core": {
+        core: {
           singleton: true,
-          import: "dist/core",
+          import: 'dist/core',
           requiredVersion: false
         },
 
