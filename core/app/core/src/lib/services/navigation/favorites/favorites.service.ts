e37322c53add57aaed1caeab2ea80985e4a78488
Clemente Raposo <clemente.raposo@salesagility.com>
2022-02-15 12:27:59 +0000

    Update front end favorite service

--- core/app/core/src/lib/services/navigation/favorites/favorites.service.ts
+++ core/app/core/src/lib/services/navigation/favorites/favorites.service.ts
@@ -72,28 +72,55 @@ export class FavoritesService {
 
                 const savedFavorite = result?.data?.favorite ?? null;
                 if (savedFavorite === null) {
+                    this.removeFavoriteFromMetadata(module, favorite);
                     return;
                 }
-
-                const saved = {
-                    id: savedFavorite?.id ?? '',
-                    module: savedFavorite?.module ?? '',
-                    attributes: {...(savedFavorite?.attributes ?? {})},
-                };
-
-                const newItemId = savedFavorite?.attributes?.parent_id ?? '';
-                const metadata = this.metadata.getModuleMeta(module);
-
-                const current = metadata?.favorites ?? null;
-                if (current) {
-                    let cleared = current.filter(item => ((item?.attributes?.parent_id ?? '') !== newItemId));
-                    cleared.unshift(saved);
-                    metadata.favorites = cleared;
-                }
-
-                this.metadata.setModuleMetadata(module, metadata);
+                this.addFavoriteToMetadata(savedFavorite, module);
             });
         }, 100);
     }
 
+    /**
+     *
+     * @param savedFavorite
+     * @param module
+     * @private
+     */
+    protected addFavoriteToMetadata(savedFavorite: Favorite, module: string): void {
+        const saved = {
+            id: savedFavorite?.id ?? '',
+            module: savedFavorite?.module ?? '',
+            attributes: {...(savedFavorite?.attributes ?? {})},
+        };
+
+        const newItemId = savedFavorite?.attributes?.parent_id ?? '';
+        const metadata = this.metadata.getModuleMeta(module);
+
+        const current = metadata?.favorites ?? null;
+        if (current) {
+            let cleared = current.filter(item => ((item?.attributes?.parent_id ?? '') !== newItemId));
+            cleared.unshift(saved);
+            metadata.favorites = cleared;
+        }
+
+        this.metadata.setModuleMetadata(module, metadata);
+    }
+
+    /**
+     * Remove favorite from metadata
+     * @param module
+     * @param favorite
+     */
+    protected removeFavoriteFromMetadata(module: string, favorite: Favorite): void {
+        const metadata = this.metadata.getModuleMeta(module);
+
+        const current = metadata?.favorites ?? null;
+        const parentId = favorite?.attributes?.parent_id ?? null;
+        if (!current || !current.length || !parentId) {
+            return;
+        }
+
+        metadata.favorites = current.filter(item => ((item?.attributes?.parent_id ?? '') !== parentId));
+        this.metadata.setModuleMetadata(module, metadata);
+    }
 }
