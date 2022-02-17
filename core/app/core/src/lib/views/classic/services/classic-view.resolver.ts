d8eb73655d92752853a82e02c0ec09767567d6b6
Clemente Raposo <clemente.raposo@salesagility.com>
2022-02-17 11:52:49 +0000

    Load module metadata on classic views
    - Load module metadata to also retrieve recently viewed records

--- core/app/core/src/lib/views/classic/services/classic-view.resolver.ts
+++ core/app/core/src/lib/views/classic/services/classic-view.resolver.ts
@@ -26,7 +26,7 @@
 
 import {Injectable} from '@angular/core';
 import {ActivatedRouteSnapshot} from '@angular/router';
-import {map, tap} from 'rxjs/operators';
+import {concatMap, map, tap} from 'rxjs/operators';
 import {ModuleNameMapper} from '../../../services/navigation/module-name-mapper/module-name-mapper.service';
 import {AppStateStore} from '../../../store/app-state/app-state.store';
 import {SystemConfigStore} from '../../../store/system-config/system-config.store';
@@ -40,6 +40,8 @@ import {MessageService} from '../../../services/message/message.service';
 import {AppMetadataStore} from '../../../store/app-metadata/app-metadata.store.service';
 import {AuthService} from '../../../services/auth/auth.service';
 import {RecentlyViewedService} from '../../../services/navigation/recently-viewed/recently-viewed.service';
+import {forkJoin} from 'rxjs';
+import {MetadataStore} from '../../../store/metadata/metadata.store.service';
 
 @Injectable({providedIn: 'root'})
 export class ClassicViewResolver extends BaseMetadataResolver {
@@ -56,7 +58,8 @@ export class ClassicViewResolver extends BaseMetadataResolver {
         protected appStateStore: AppStateStore,
         protected appMetadata: AppMetadataStore,
         protected auth: AuthService,
-        protected recentlyViewed: RecentlyViewedService
+        protected recentlyViewed: RecentlyViewedService,
+        protected metadataStore: MetadataStore,
     ) {
         super(
             systemConfigStore,
@@ -73,8 +76,13 @@ export class ClassicViewResolver extends BaseMetadataResolver {
     }
 
     resolve(route: ActivatedRouteSnapshot): any {
-
+        const module = this.calculateActiveModule(route);
         return super.resolve(route).pipe(
+            concatMap(() => {
+                return forkJoin({
+                    metadata: this.metadataStore.load(module, this.metadataStore.getMetadataTypes()),
+                });
+            }),
             map(() => this.routeConverter.toLegacy(route.params, route.queryParams)),
             tap(
                 () => {
