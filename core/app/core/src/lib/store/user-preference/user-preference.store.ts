41324016df141f1befbd628320a567e149fc9fb7
Clemente Raposo <clemente.raposo@salesagility.com>
2022-02-23 18:04:42 +0000

    Add ui user preferences frontend handling
    - Enable saving ui user preferences to the backend

--- core/app/core/src/lib/store/user-preference/user-preference.store.ts
+++ core/app/core/src/lib/store/user-preference/user-preference.store.ts
@@ -25,12 +25,15 @@
  */
 
 import {Injectable} from '@angular/core';
-import {BehaviorSubject, Observable, of} from 'rxjs';
-import {distinctUntilChanged, map, shareReplay, tap} from 'rxjs/operators';
+import {BehaviorSubject, Observable, of, Subscription} from 'rxjs';
+import {debounceTime, distinctUntilChanged, map, shareReplay, take, tap} from 'rxjs/operators';
 
 import {CollectionGQL} from '../../services/api/graphql-api/api.collection.get';
 import {deepClone} from 'common';
 import {StateStore} from '../state';
+import {SystemConfigStore} from '../system-config/system-config.store';
+import {ProcessService} from '../../services/process/process.service';
+import {LocalStorageService} from '../../services/local-storage/local-storage.service';
 
 export interface UserPreferenceMap {
     [key: string]: any;
@@ -56,6 +59,9 @@ let cache$: Observable<any> = null;
 export class UserPreferenceStore implements StateStore {
     protected store = new BehaviorSubject<UserPreferences>(internalState);
     protected state$ = this.store.asObservable();
+    protected saveBufferStore = new BehaviorSubject<boolean>(false);
+    protected saveBuffer$: Observable<boolean>;
+    protected subs: Subscription[] = [];
     protected resourceName = 'userPreferences';
     protected fieldsMetadata = {
         fields: [
@@ -72,7 +78,21 @@ export class UserPreferenceStore implements StateStore {
     userPreferences$ = this.state$.pipe(map(state => state.userPreferences), distinctUntilChanged());
     loading$ = this.state$.pipe(map(state => state.loading));
 
-    constructor(private collectionGQL: CollectionGQL) {
+    constructor(
+        protected collectionGQL: CollectionGQL,
+        protected config: SystemConfigStore,
+        protected processService: ProcessService,
+        protected localStorage: LocalStorageService,
+    ) {
+        const uiSettings = config.getConfigValue('ui') ?? {};
+        const delay = uiSettings.user_preferences_save_delay ?? 5000;
+        this.saveBuffer$ = this.saveBufferStore.asObservable().pipe(debounceTime(delay ?? 5000));
+        this.subs.push(this.saveBuffer$.subscribe((value) => {
+            if (!value) {
+                return;
+            }
+            this.saveUiPreferences();
+        }));
     }
 
     /**
@@ -85,6 +105,7 @@ export class UserPreferenceStore implements StateStore {
     public clear(): void {
         cache$ = null;
         this.updateState(deepClone(initialState));
+        this.subs.forEach(sub => sub.unsubscribe());
     }
 
     public clearAuthBased(): void {
@@ -104,6 +125,94 @@ export class UserPreferenceStore implements StateStore {
         }
 
         return internalState.userPreferences[key];
+    }
+
+    /**
+     * Get ui user preferences value by key
+     *
+     * @param module
+     * @param {string} key to retrieve
+     * @returns any users preference
+     */
+    public getUi(module: string, key: string): any {
+
+        const storageKey = module + '-' + key;
+        const value = this.storageLoad(module, storageKey);
+
+        if (value != null) {
+            return value;
+        }
+
+        const ui = internalState?.userPreferences?.ui ?? {} as any;
+        return ui[storageKey] ?? null;
+    }
+
+    /**
+     * Set user preferences value by key
+     *
+     * @param {string} module name
+     * @param {string} key to retrieve
+     * @param value
+     * @returns any users preference
+     */
+    public setUi(module: string, key: string, value: any): void {
+
+        const storageKey = module + '-' + key;
+        this.storageSave(module, storageKey, value);
+
+        const ui = internalState?.userPreferences?.ui ?? {} as any;
+        ui[storageKey] = value;
+
+        internalState.userPreferences.ui = ui;
+
+        this.saveBufferStore.next(true);
+    }
+
+    protected saveUiPreferences(): void {
+
+        const processType = 'save-ui-user-preferences';
+
+        const options = {
+            preferences: internalState.userPreferences.ui
+        };
+
+
+        this.processService.submit(processType, options).pipe(take(1)).subscribe();
+    }
+
+    /**
+     * Store the data in local storage
+     *
+     * @param {string} module to store in
+     * @param {string} storageKey to store in
+     * @param {} data to store
+     */
+    protected storageSave(module: string, storageKey: string, data: any): void {
+        let storage = this.localStorage.get(storageKey);
+
+        if (!storage) {
+            storage = {};
+        }
+
+        storage[module] = data;
+
+        this.localStorage.set(storageKey, storage);
+    }
+
+    /**
+     * Store the key in local storage
+     *
+     * @param {string} module to load from
+     * @param {string} storageKey from load from
+     */
+    protected storageLoad(module: string, storageKey: string): any {
+        const storage = this.localStorage.get(storageKey);
+
+        if (!storage || !storage[module]) {
+            return null;
+        }
+
+        return storage[module];
     }
 
 
