804972c3f6b585f1b20816cf99e9f73ba2a8a69a
Clemente Raposo <clemente.raposo@salesagility.com>
2022-05-17 18:37:50 +0100

    Squashed 'public/legacy/' changes from b065977c61..5750e66a06
    5750e66a06 SuiteCRM 7.12.6 Release
    d80ca53e48 Update jquery UI to v1.13.1
    cacf7aa247 Upgrade jstree to v3.3.12
    2c95a4c083 Update Wysiwyg field to work with the new version of TinyMCE
    9c487fa43c Update email/template views to work with new versions of TinyMCE
    7bcbf2a737 Upgrade TinyMCE to version 5.10
    63b89fbd4f Fix #9494 - Force displaying line breaks to textarea fields
    63dc9adf21 Fix #9530 - Fatal error on rendering page after upload
    59d0b42cf0 Fix Module Builder Acceptance Tests
    29f18cf248 Fix AOR_Report unit tests
    ccbb40ca4a Fix scrm-core#87 - Prevent disabling the default lang
    4c83fe64a3 Fix #9437 - Use application setCookie across the app
    b293490c65 Fix #9437 - Change cookie_path handling
    4e8924e549 Fix #3157 - Add default option to enable session_gc
    120e948307 Fix #9438 - Adding Action keyword to fieldname exception
    e037232900 Fix #8599 - Auto-close message boxes in ModuleBuilder
    277d40b61a Fix #9435 - Dropdown doesn't return empty selected value
    
    git-subtree-dir: public/legacy
    git-subtree-split: 5750e66a060310494cc12b074062ffd36658bc1e

--- suitecrm_version.php
+++ suitecrm_version.php
@@ -4,5 +4,5 @@ if (!defined('sugarEntry') || !sugarEntry) {
 }
 
 $suitecrm_version = '8.0.1';
-$suitecrm_legacy = '7.12.5';
-$suitecrm_timestamp = '2022-03-01 12:00:00';
+$suitecrm_legacy = '7.12.6';
+$suitecrm_timestamp = '2022-05-19 12:00:00';
