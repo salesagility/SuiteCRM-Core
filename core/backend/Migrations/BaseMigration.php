49add20c8a55cb221d2b674d1ed7be2e5018af45
Clemente Raposo <clemente.raposo@salesagility.com>
2022-09-12 11:02:59 +0100

    Add base migration
    - Move common migration handling methods to BaseMigration class

--- core/backend/Migrations/BaseMigration.php
+++ core/backend/Migrations/BaseMigration.php
@@ -25,17 +25,15 @@
  * the words "Supercharged by SuiteCRM".
  */
 
-declare(strict_types=1);
-
 namespace App\Migrations;
 
-use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
 use Psr\Log\LoggerInterface;
-use Symfony\Component\DependencyInjection\ContainerAwareInterface;
 use Symfony\Component\DependencyInjection\ContainerInterface;
 
-final class Version20220907112919 extends BaseMigration implements ContainerAwareInterface
+abstract class BaseMigration extends AbstractMigration
 {
+
     /**
      * @var ContainerInterface
      */
@@ -46,44 +44,39 @@ final class Version20220907112919 extends BaseMigration implements ContainerAwar
      */
     protected $upgradeLogger;
 
-    public function getDescription(): string
+    public function setContainer(ContainerInterface $container = null): void
     {
-        return 'Add LOCK_DSN to .env';
+        $this->container = $container;
     }
 
-    public function isTransactional(): bool
+    protected function getProjectDir(): string
     {
-        return false;
+        return $this->container->getParameter('kernel.project_dir');
     }
 
-    public function up(Schema $schema): void
+    protected function log(string $message): void
     {
-        $envFile = $this->getProjectDir() . "/.env";
-
-        if (!file_exists($envFile)) {
+        $logger = $this->getUpgradeLogger();
+        if ($logger === null) {
             return;
         }
 
-        $envContents = file_get_contents($envFile);
-
-        if (strpos($envContents, 'LOCK_DSN')) {
-            $this->upgradeLogger->info('.env already contains LOCK_DSN. Skipping update.');
-
-            return;
-        }
-
-        $envContents .= "\n";
-        $envContents .= "###> symfony/lock ###\n";
-        $envContents .= "LOCK_DSN=flock\n";
-        $envContents .= "###< symfony/lock ###\n";
-        $envContents .= "\n";
-
-        file_put_contents($envFile, $envContents);
-
-        $this->log('Added LOCK_DSN to .env.');
+        $logger->info($message);
     }
 
-    public function down(Schema $schema): void
+    protected function getUpgradeLogger(): ?LoggerInterface
     {
+        if ($this->upgradeLogger !== null) {
+            return $this->upgradeLogger;
+        }
+
+        $logger = $this->container->get('monolog.logger.upgrade');
+        if ($logger instanceof LoggerInterface) {
+            $this->upgradeLogger = $logger;
+
+            return $logger;
+        }
+
+        return null;
     }
 }
