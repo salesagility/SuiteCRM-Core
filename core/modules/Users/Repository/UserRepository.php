6bbd75d8f1be9c6b6f8cb8d3e535ac217dbdcdfc
Jack Anderson <jack.anderson@salesagility.com>
2022-11-28 10:56:02 +0000

    Add check for active user

--- core/modules/Users/Repository/UserRepository.php
+++ core/modules/Users/Repository/UserRepository.php
@@ -56,6 +56,7 @@ class UserRepository extends EntityRepository implements UserLoaderInterface
     {
         return $this->createQueryBuilder('u')
             ->where('u.user_name = :user_name')
+            ->andWhere("u.status = 'active'")
             ->andWhere('u.deleted = :deleted')
             ->setParameter('user_name', $username)
             ->setParameter('deleted', 0)
