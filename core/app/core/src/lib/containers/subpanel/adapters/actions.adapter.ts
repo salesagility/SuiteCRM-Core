a3690857d36d5600b0bd22ab8ce1b2eecaacefd6
Jack Anderson <jack.anderson@salesagility.com>
2023-01-30 11:52:44 +0000

    Add actions adapter for subpanels

---
+++ core/app/core/src/lib/containers/subpanel/adapters/actions.adapter.ts
@@ -0,0 +1,111 @@
+/**
+ * SuiteCRM is a customer relationship management program developed by SalesAgility Ltd.
+ * Copyright (C) 2023 SalesAgility Ltd.
+ *
+ * This program is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Affero General Public License version 3 as published by the
+ * Free Software Foundation with the addition of the following permission added
+ * to Section 15 as permitted in Section 7(a): FOR ANY PART OF THE COVERED WORK
+ * IN WHICH THE COPYRIGHT IS OWNED BY SALESAGILITY, SALESAGILITY DISCLAIMS THE
+ * WARRANTY OF NON INFRINGEMENT OF THIRD PARTY RIGHTS.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Affero General Public License for more
+ * details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * In accordance with Section 7(b) of the GNU Affero General Public License
+ * version 3, these Appropriate Legal Notices must retain the display of the
+ * "Supercharged by SuiteCRM" logo. If the display of the logos is not reasonably
+ * feasible for technical reasons, the Appropriate Legal Notices must display
+ * the words "Supercharged by SuiteCRM".
+ */
+
+import {Injectable} from '@angular/core';
+import {Action, ActionContext, ViewMode} from 'common';
+import {combineLatest, Observable} from 'rxjs';
+import {map, take} from 'rxjs/operators';
+import {AsyncActionService} from '../../../services/process/processes/async-action/async-action';
+import {LanguageStore} from '../../../store/language/language.store';
+import {MessageService} from '../../../services/message/message.service';
+import {Process} from '../../../services/process/process.service';
+import {ConfirmationModalService} from '../../../services/modals/confirmation-modal.service';
+import {BaseRecordActionsAdapter} from '../../../services/actions/base-record-action.adapter';
+import {SelectModalService} from "../../../services/modals/select-modal.service";
+import {MetadataStore} from '../../../store/metadata/metadata.store.service';
+import {SubpanelActionManager} from "../components/subpanel/action-manager.service";
+import {SubpanelActionData} from "../actions/subpanel.action";
+import {SubpanelStore} from "../store/subpanel/subpanel.store";
+
+@Injectable()
+export class SubpanelActionsAdapter extends BaseRecordActionsAdapter<SubpanelActionData> {
+
+    constructor(
+        protected store: SubpanelStore,
+        protected language: LanguageStore,
+        protected actionManager: SubpanelActionManager,
+        protected asyncActionService: AsyncActionService,
+        protected message: MessageService,
+        protected confirmation: ConfirmationModalService,
+        protected selectModalService: SelectModalService,
+        protected metadata: MetadataStore
+    ) {
+        super(
+            actionManager,
+            asyncActionService,
+            message,
+            confirmation,
+            language,
+            selectModalService,
+            metadata
+        )
+    }
+
+    getActions(context?: ActionContext): Observable<Action[]> {
+        return combineLatest(
+            [
+                this.store.metadata$,
+                this.language.vm$,
+                this.store.criteria$
+            ]
+        ).pipe(
+            map((
+                [
+                    meta
+                ]
+            ) => {
+                if (!meta) {
+                    return [];
+                }
+
+                return this.parseModeActions(meta.top_buttons, "list" as ViewMode);
+            })
+        );
+    }
+
+    protected buildActionData(action: Action, context?: ActionContext): SubpanelActionData {
+        return {
+            subpanelMeta: this.store?.metadata,
+            module: action.module || this.store.metadata?.module,
+            parentModule: this.store.parentModule,
+            parentId: this.store.parentId,
+            store: this.store,
+            action
+        } as SubpanelActionData;
+    }
+
+    protected getMode(): ViewMode {
+        return 'list';
+    }
+
+    protected getModuleName(context?: ActionContext): string {
+        return this.store?.metadata?.module;
+    }
+
+    protected reload(action: Action, process: Process, context?: ActionContext): void {
+        this.store.load(false).pipe(take(1)).subscribe();
+    }
+}
--- core/app/core/src/lib/containers/subpanel/components/subpanel/subpanel.component.ts
+++ core/app/core/src/lib/containers/subpanel/components/subpanel/subpanel.component.ts
@@ -25,9 +25,8 @@
  */
 
 import {Component, Input, OnInit} from '@angular/core';
-import {Action, AnyButtonInterface, ButtonGroupInterface, ButtonInterface, isFalse} from 'common';
-import {Observable, of} from 'rxjs';
-import {shareReplay} from 'rxjs/operators';
+import {ButtonGroupInterface, ButtonInterface} from 'common';
+import {Observable} from 'rxjs';
 import {TableConfig} from '../../../../components/table/table.model';
 import {SubpanelTableAdapter} from '../../adapters/table.adapter';
 import {LanguageStore} from '../../../../store/language/language.store';
@@ -39,6 +38,8 @@ import {SystemConfigStore} from "../../../../store/system-config/system-config.s
 import {FilterConfig} from "../../../list-filter/components/list-filter/list-filter.model";
 import {SubpanelFilterAdapterFactory} from "../../adapters/filter.adapter.factory";
 import {SubpanelFilterAdapter} from "../../adapters/filter.adapter";
+import {SubpanelActionAdapterFactory} from "../../adapters/actions.adapter.factory";
+import {SubpanelActionsAdapter} from "../../adapters/actions.adapter";
 
 @Component({
     selector: 'scrm-subpanel',
@@ -58,6 +59,7 @@ export class SubpanelComponent implements OnInit {
     config$: Observable<ButtonGroupInterface>;
     tableConfig: TableConfig;
     filterAdapter: SubpanelFilterAdapter;
+    actionsAdapter: SubpanelActionsAdapter;
 
     constructor(
         protected actionManager: SubpanelActionManager,
@@ -65,15 +67,15 @@ export class SubpanelComponent implements OnInit {
         protected tableAdapterFactory: SubpanelTableAdapterFactory,
         protected preferences: UserPreferenceStore,
         protected systemConfigs: SystemConfigStore,
-        protected filterAdapterFactory: SubpanelFilterAdapterFactory
+        protected filterAdapterFactory: SubpanelFilterAdapterFactory,
+        protected actionAdapterFactory: SubpanelActionAdapterFactory
     ) {
     }
 
     ngOnInit(): void {
-        this.adapter = this.tableAdapterFactory.create(this.store);
-        this.tableConfig = this.adapter.getTable();
-        this.filterAdapter = this.filterAdapterFactory.create(this.store);
-        this.filterConfig = this.filterAdapter.getConfig();
+
+        this.buildAdapters();
+
         if (this.maxColumns$) {
             this.tableConfig.maxColumns$ = this.maxColumns$;
         }
@@ -98,8 +100,6 @@ export class SubpanelComponent implements OnInit {
             this.store.recordList.updateSorting(sort.orderBy, sort.sortOrder);
         }
 
-        this.config$ = of(this.getButtonGroupConfig(this.buildAction())).pipe(shareReplay(1));
-
         this.closeButton = {
             onClick: (): void => {
                 this.onClose && this.onClose();
@@ -107,77 +107,11 @@ export class SubpanelComponent implements OnInit {
         } as ButtonInterface;
     }
 
-    buildAction(): any {
-        const actions = [];
-
-        if (this.store.metadata) {
-            if (this.store.metadata.top_buttons) {
-                this.store.metadata.top_buttons.forEach(button => {
-                    const label = this.languages.getFieldLabel(
-                        button.labelKey,
-                        button.module
-                    );
-
-                    actions.push({
-                        ...button,
-                        label,
-                        params: {
-                            module: button.module
-                        }
-                    });
-                });
-            }
-        }
-
-        return actions;
-    }
-
-    getButtonGroupConfig(actions: Action[]): ButtonGroupInterface {
-        const buttons = [];
-
-        actions.forEach((action: Action) => {
-            buttons.push(this.buildButton(action));
-        });
-
-        let breakpoint = 1;
-        if (buttons && buttons.length > 1) {
-            breakpoint = -1;
-        }
-
-        const dropdownLabel = this.languages.getAppString('LBL_ACTIONS');
-
-        return {
-            buttons,
-            breakpoint,
-            dropdownLabel,
-            buttonKlass: ['btn', 'btn-sm', 'btn-outline-light']
-        } as ButtonGroupInterface;
-    }
-
-    protected buildButton(action: Action): AnyButtonInterface {
-        const button = {
-            label: action.label || '',
-            klass: 'btn btn-sm btn-outline-light',
-            onClick: (): void => {
-                this.actionManager.run(action.key, {
-                    subpanelMeta: this.store.metadata,
-                    module: action.params.module || this.store.metadata.module,
-                    parentModule: this.store.parentModule,
-                    parentId: this.store.parentId,
-                    store: this.store,
-                    action
-                });
-            }
-        } as AnyButtonInterface;
-
-        const debounceClick = action?.params?.debounceClick ?? null;
-
-        button.debounceClick = true;
-
-        if (isFalse(debounceClick)) {
-            button.debounceClick = false;
-        }
-
-        return button;
+    buildAdapters(): void {
+        this.adapter = this.tableAdapterFactory.create(this.store);
+        this.tableConfig = this.adapter.getTable();
+        this.filterAdapter = this.filterAdapterFactory.create(this.store);
+        this.filterConfig = this.filterAdapter.getConfig();
+        this.actionsAdapter = this.actionAdapterFactory.create(this.store);
     }
 }
