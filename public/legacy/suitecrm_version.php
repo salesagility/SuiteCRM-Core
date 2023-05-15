2c1f3e092a7533b704e87c3705e563fbe1dcdb5e
Jack Anderson <jack.anderson@salesagility.com>
2023-05-15 13:08:12 +0100

    [Legacy] Bump version to 8.3.0

--- public/legacy/suitecrm_version.php
+++ public/legacy/suitecrm_version.php
@@ -3,6 +3,6 @@ if (!defined('sugarEntry') || !sugarEntry) {
     die('Not A Valid Entry Point');
 }
 
-$suitecrm_version = '8.3-beta';
-$suitecrm_timestamp = '2023-04-24 12:00:00';
+$suitecrm_version = '8.3.0';
+$suitecrm_timestamp = '2023-05-16 12:00:00';
 $suitecrm_legacy = '7.13.3';
