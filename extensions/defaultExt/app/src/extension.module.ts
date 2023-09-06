2dd964bed70535c43ab0b4a66ce2445d4417707a
Clemente Raposo <clemente.raposo@salesagility.com>
2023-09-06 17:22:07 +0100

    Revert changes to defaultExt

--- extensions/defaultExt/app/src/extension.module.ts
+++ extensions/defaultExt/app/src/extension.module.ts
@@ -1,29 +1,12 @@
 import {NgModule} from '@angular/core';
-import {CommonModule} from '@angular/common';
-import {HttpClientModule} from '@angular/common/http';
-import {UKPhoneEditFieldComponent} from './fields/uk-phone/templates/edit/uk-phone.component';
-import {UkPhoneEditFieldModule} from './fields/uk-phone/templates/edit/uk-phone.module';
-import {FieldRegistry} from 'core';
-
 
 @NgModule({
     declarations: [],
-    imports: [
-        CommonModule,
-        HttpClientModule,
-        UkPhoneEditFieldModule
-    ],
+    imports: [],
     providers: []
 })
 export class ExtensionModule {
-    constructor(
-        protected fieldRegistry: FieldRegistry
-    ) {
-        // Override the edit mode phone field component for all modules
-        fieldRegistry.register('default', 'phone', 'edit', UKPhoneEditFieldComponent);
-
-        // Override the edit mode phone field component just for accounts
-        // fieldRegistry.register('accounts', 'phone', 'edit', UKPhoneEditFieldComponent);
+    constructor() {
     }
 
     init(): void {
