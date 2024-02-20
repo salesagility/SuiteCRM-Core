4766288ac87e7342fa16a46065e4f32711ba9e6c
Jack Anderson <jack.anderson@salesagility.com>
2024-02-20 13:36:06 +0000

    Update check table build migration

--- core/backend/Migrations/Version20231117113210.php
+++ core/backend/Migrations/Version20231117113210.php
@@ -1,16 +1,42 @@
 <?php
+/**
+ * SuiteCRM is a customer relationship management program developed by SalesAgility Ltd.
+ * Copyright (C) 2024 SalesAgility Ltd.
+ *
+ * This program is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Affero General Public License version 3 as published by the
+ * Free Software Foundation with the addition of the following permission added
+ * to Section 15 as permitted in Section 7(a): FOR ANY PART OF THE COVERED WORK
+ * IN WHICH THE COPYRIGHT IS OWNED BY SALESAGILITY, SALESAGILITY DISCLAIMS THE
+ * WARRANTY OF NON INFRINGEMENT OF THIRD PARTY RIGHTS.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Affero General Public License for more
+ * details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * In accordance with Section 7(b) of the GNU Affero General Public License
+ * version 3, these Appropriate Legal Notices must retain the display of the
+ * "Supercharged by SuiteCRM" logo. If the display of the logos is not reasonably
+ * feasible for technical reasons, the Appropriate Legal Notices must display
+ * the words "Supercharged by SuiteCRM".
+ */
 
 declare(strict_types=1);
 
 namespace App\Migrations;
 
-use CacheManager;
 use Doctrine\DBAL\Schema\Schema;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\DependencyInjection\ContainerAwareInterface;
 
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20231117113210 extends BaseMigration
+final class Version20231117113210 extends BaseMigration implements ContainerAwareInterface
 {
 
     public function getDescription(): string
@@ -20,16 +46,15 @@ final class Version20231117113210 extends BaseMigration
 
     public function up(Schema $schema): void
     {
+        /** @var EntityManagerInterface $entityManager */
+        $entityManager = $this->container->get('entity_manager');
 
-        require_once "include/portability/Services/Cache/CacheManager.php";
-        $tableExists = (new CacheManager())->checkIfCacheTableExists();
-
-        if ($tableExists) {
-            $this->log('Cache Table Exists');
-            return;
+        try {
+            $entityManager->getConnection()->executeQuery('CREATE TABLE cache_rebuild (cache_key varchar(255), rebuild boolean)');
+        } catch (\Exception $e) {
+            $this->log('cache_rebuild table already exists');
+            $this->log($e->getMessage());
         }
-
-        $this->log('Could not create cache table');
     }
 
     public function down(Schema $schema): void
