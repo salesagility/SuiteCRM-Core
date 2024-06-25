fb8be46256085dcf8d8bbc499e1a06ab00c3e7f4
Jack Anderson <jack.anderson@salesagility.com>
2024-06-25 13:23:48 +0100

    Move install check to autoload runtime template

--- public/index.php
+++ public/index.php
@@ -4,11 +4,6 @@ use App\Kernel;
 
 require_once dirname(__DIR__) . '/vendor/autoload_runtime.php';
 
-if (!file_exists('legacy/config.php') && !file_exists('../.installed_checked') && !file_exists('../.curl_check_main_page')){
-    header('Location: install.php');
-    return;
-}
-
 return function (array $context) {
     return new Kernel($context['APP_ENV'], (bool)$context['APP_DEBUG']);
 };
