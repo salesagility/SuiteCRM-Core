433b8c75253c0f4150d24651b8f032f227ad42e3
Jack Anderson <jack.anderson@suitecrm.com>
2026-06-22 15:23:21 +0100

    Add Pre Upgrade Migration Run All Command

---
+++ core/backend/Install/Command/PreUpgradeMigrations/RunAllPreUpgradeMigrationsCommand.php
@@ -0,0 +1,48 @@
+<?php
+/**
+ * SuiteCRM is a customer relationship management program developed by SuiteCRM Ltd.
+ * Copyright (C) 2026 SuiteCRM Ltd.
+ *
+ * This program is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Affero General Public License version 3 as published by the
+ * Free Software Foundation with the addition of the following permission added
+ * to Section 15 as permitted in Section 7(a): FOR ANY PART OF THE COVERED WORK
+ * IN WHICH THE COPYRIGHT IS OWNED BY SUITECRM, SUITECRM DISCLAIMS THE
+ * WARRANTY OF NON INFRINGEMENT OF THIRD PARTY RIGHTS.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Affero General Public License for more
+ * details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * In accordance with Section 7(b) of the GNU Affero General Public License
+ * version 3, these Appropriate Legal Notices must retain the display of the
+ * "Supercharged by SuiteCRM" logo. If the display of the logos is not reasonably
+ * feasible for technical reasons, the Appropriate Legal Notices must display
+ * the words "Supercharged by SuiteCRM".
+ */
+
+namespace App\Install\Command\PreUpgradeMigrations;
+
+use Symfony\Component\Console\Attribute\AsCommand;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+
+#[AsCommand(name: 'suitecrm:pre-upgrade-migrations:run-all')]
+class RunAllPreUpgradeMigrationsCommand extends BasePreUpgradeMigrationCommand
+{
+    protected function configure(): void
+    {
+        $this->setDescription('Run all pending pre-upgrade migrations');
+    }
+
+    protected function execute(InputInterface $input, OutputInterface $output): int
+    {
+        $feedback = $this->runner->run($this->getMigrationsDir());
+
+        return $this->writeFeedback($output, $feedback);
+    }
+}
